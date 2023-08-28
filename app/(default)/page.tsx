a2d1a6e3a92468bb6c06b3c9903a3ceab5ec919c
Daurius Swarts <ali@openmindi.co.za>
2023-08-28 15:52:14 +0200

    [FE] Adding Reviews

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <Hero />
       <About />
       {/* <Features /> */}
-      {/* <Testimonials /> */}
       <Medium />
+      <Testimonials />
       <Contact />
     </>
   );
